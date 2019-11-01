731a53d5ae5fb822021f5c85f59c3897ad15333a
nikolay.kapustin <nikolay.kapustin@HWTOOL.NET>
2019-11-01 16:44:23 +0200

    Task 3 complete

--- source/server.js
+++ source/server.js
@@ -1,7 +1,7 @@
 // Core
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getPassword } from './utils';
+import { getPassword, NotFoundError } from './utils';
 
 //Routers
 import * as routers from './routers';
@@ -13,10 +13,16 @@ app.use(bodyParser.json({ limit: '10kb' }));
 // Check password
 app.use(getPassword);
 
+// Check 404
+
 // Routers
 app.use('/auth', routers.auth);
 app.use('/users', routers.users);
 app.use('/classes', routers.classes);
 app.use('/lessons', routers.lessons);
+
+app.use('*', (req, res, next) => {
+    next(new NotFoundError(`Cant find method: ${req.method} and path: ${req.originalUrl}`));
+});
 
 export { app };
